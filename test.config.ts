bcba858cd6674936465613761d5374f24372a278
Apoorv <apoorv9221@gmail.com>
2025-08-30 10:35:21 +0530

    xyz

---
+++ test.config.ts
@@ -0,0 +1,15 @@
+export class TestConfig{
+ 
+    //appUrl="http://localhost/opencart/upload/"
+    //appUrl="https://naveenautomationlabs.com/opencart"
+    appUrl="https://tutorialsninja.com/demo/"
+
+//valid login credentials- create your own login account
+email="sdf@e.com"
+password="test@123"
+
+//product details
+productName="MacBook"
+productQuantity="2"
+totalPrice="$1,204.00"
+}
